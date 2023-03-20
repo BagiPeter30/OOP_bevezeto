4e0fa91aae8a90e530c2d6cbfbad59c7d6a6e466
bsze2bagpet <bsze2bagpet@SZT1-17-HP>
2023-03-20 17:47:25 +0100

    myCatpeldany

--- bev-projekt/bev-projekt/Program.cs
+++ bev-projekt/bev-projekt/Program.cs
@@ -42,5 +42,20 @@ public class Dog
 
 }
 
+///Program.cs - főprogram
+
+            // O1.05 OOP programozás
+            // Az OOP programozás során a programokat objektumokból építjük fel.
+            //  - Minden objektumnak megvan a jól meghatározott feladata.
+            //  - Az objektumok képesek egymással kommunikálni.
+            //  - OOP programozás során a programozást közelítjük az emberi gondolkodáshoz.
+
+// O1.06 Példányosítás
+// Cat osztály példányosítása (create instance of class).
+//  - A myCat objektum a Cat osztály egy példánya.
+//  - A lokális változókat írásakor a caneCaseing jelölést használunk.
+            Cat myCat = new Cat();
+
+
 
 
